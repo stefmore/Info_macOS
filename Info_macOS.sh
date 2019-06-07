a9bef245c26701b3533a3b85fdd5ecf59e0d2406
stefmore <36001134+stefmore@users.noreply.github.com>
2019-06-07 20:07:34 +0200

    Update Info_macOS.sh

--- Info_macOS.sh
+++ Info_macOS.sh
@@ -4,7 +4,15 @@
 CN=`networksetup -getcomputername`
 
 #cpuinfo = model+clock
-cpuinfo=`sysctl -n machdep.cpu.brand_string`
+cpuinfo=`sysctl -n machdep.cpu.brand_string | awk '{print$2}'`
+core=`sysctl hw.physicalcpu | awk '{print$2}'`
+
+#Mac model
+model=`curl -s https://support-sp.apple.com/sp/product?cc=$(
+  system_profiler SPHardwareDataType \
+    | awk '/Serial/ {print $4}' \
+    | cut -c 9-
+) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|'`
 
 # Distro
 distro="macOS"
@@ -16,8 +24,8 @@ memory=`system_profiler SPHardwareDataType | grep "Memory" | awk '{print$2,$3}'`
 #storage info
 storage=`system_profiler SPStorageDataType | grep "Capacity" | awk '{print$2,$3}'NR==1{exit}`
 medium=`system_profiler SPStorageDataType | grep "Medium" | awk '{print$3}'`
-device=`system_profiler SPStorageDataType | grep "Device" | awk '{print$4,$5}'`
+device=`system_profiler SPStorageDataType | grep "Device" | awk '{print$3}'`
 file=`system_profiler SPStorageDataType | grep "File" | awk '{print$3}'`
 available=`system_profiler SPStorageDataType | grep "Available" | awk '{print$2,$3}'`
 
-echo "$CN|$distro $version|$cpuinfo|$memory|$file|$storage $medium|$available"
+echo "$CN|$model|$distro $version|$cpuinfo|$core cores|$memory|$file|$device|$storage|$medium|$available"
